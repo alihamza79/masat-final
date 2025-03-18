182b2b1249db4b31cbc3c5a9e7edb77fb1718c16
Tamas Vlad <vlad.tamas@accesa.eu>
2025-03-18 22:30:20 +0200

    Add host

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,6 +40,7 @@ provider "aws" {
 }
 
 provider "docker" {
+  host = "unix:///var/run/docker.sock"
   registry_auth {
     address  = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.current.name)
     username = data.aws_ecr_authorization_token.token.user_name
